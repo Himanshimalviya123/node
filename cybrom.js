3cbe6d85616e5de983da746d2ffa71deac0c86ad
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-07-17 22:52:33 +0530

    update

--- cybrom.js
+++ cybrom.js
@@ -1,10 +1,10 @@
-const Myname=()=>{
-    return"hi am himanshi "
-}
-const Myclass=()=>{
-    return"welcome to node js class"
-}
-module.exports={
-Myname,
-Myclass
-}
+// const Myname=()=>{
+//     return"hi am himanshi "
+// }
+// const Myclass=()=>{
+//     return"welcome to node js class"
+// }
+// module.exports={
+// Myname,
+// Myclass
+// }
