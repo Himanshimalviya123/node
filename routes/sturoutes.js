ac7bf4c39e3a9034429c0b6d5d3da612d4d92a59
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-07-19 23:16:16 +0530

    update

--- routes/sturoutes.js
+++ routes/sturoutes.js
@@ -3,5 +3,14 @@ const route=express.Router();
 route.get("/home",(req,res)=>{
     res.send("<h1>welcome to home page</h1>")
 })
+route.get("/course",(req,res)=>{
+    res.send("<h1>this is our course</h1>")
+})
+route.get("/department",(req,res)=>{
+    res.send("<h1>i am cse department</h1>")
+})
+route.get("/fees",(req,res)=>{
+    res.send("<h1>this is our fees<h1>")
+})
 
 module.exports=route;
